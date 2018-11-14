b2387cd3063ac8f43b5f76c19457edf8bccedc01
Peter Dimitri <golem65@gmx.de>
2018-11-14 17:19:34 +0100

    correct condition check

--- src/App.js
+++ src/App.js
@@ -50,15 +50,11 @@ class App extends Component {
       cursor: 'pointer'
     };
 
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App!</h1>
-        <button 
-        style={style}
-        onClick={this.togglePersonsHandler}>Toggle Persons</button>
-         {
-         this.state.showPersons ? 
-          <div>
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (
+        <div>
             <Person 
             name={this.state.persons[0].name} 
             age={this.state.persons[0].age}
@@ -75,9 +71,18 @@ class App extends Component {
             >
               My Hobby is Racing
             </Person>
-          </div>
-         : null
-        }
+          </div>  
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App!</h1>
+        <button 
+        style={style}
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+         
+      {persons}    
       </div>
       
     );
