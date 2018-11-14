4cf95a343ae684ed7fbe70d161e86475fb60b3d7
Peter Dimitri <golem65@gmx.de>
2018-11-14 16:43:03 +0100

    End of Chapter 3

--- src/App.js
+++ src/App.js
@@ -23,11 +23,34 @@ class App extends Component {
     });
   }
 
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Max', age: '28'},
+        {name: event.target.value, age: '34'},
+        {name: 'Stephanie', age: '17'}
+      ]
+    });
+  }
+
+
   render() {
+    //Inline Style - at the button element - doesn't give the full power of CSS. Example: Hover State
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
-        <button onClick={() => this.switchNameHandler('Volker')}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Volker')}>Switch Name</button>
         <Person 
         name={this.state.persons[0].name} 
         age={this.state.persons[0].age}
@@ -35,11 +58,13 @@ class App extends Component {
         <Person 
         name={this.state.persons[1].name} 
         age={this.state.persons[1].age}
+        changed={this.nameChangedHandler}
         />
         <Person 
         name={this.state.persons[2].name} 
         age={this.state.persons[2].age}
-        click = {this.switchNameHandler.bind(this,'Harry')}>
+        click = {this.switchNameHandler.bind(this,'Harry')}
+        >
           My Hobby is Racing
         </Person>
       </div>
